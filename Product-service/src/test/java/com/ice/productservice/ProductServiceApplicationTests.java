b2f65620d9b52de531219ef0490d0b77a8fff64e
icetruong <hocthidauthoi@gmail.com>
2026-05-15 08:53:01 +0700

    feat: initialize Spring Boot project structure for product-service

---
+++ Product-service/src/test/java/com/ice/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ice.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
