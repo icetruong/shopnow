f914f670e5afe1b324a8cbcce9d29a92dda8f097
icetruong <hocthidauthoi@gmail.com>
2026-05-16 19:23:42 +0700

    feat: implement product variant management with CRUD services, DTOs, and global exception handling

---
+++ Product-service/src/main/java/com/ice/productservice/Exception/VariantInActiveOrderException.java
@@ -0,0 +1,7 @@
+package com.ice.productservice.Exception;
+
+public class VariantInActiveOrderException extends RuntimeException {
+  public VariantInActiveOrderException(String message) {
+    super(message);
+  }
+}
