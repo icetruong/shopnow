cc4bdd96ac8cfad09e5510722e0c95016e1c713f
icetruong <hocthidauthoi@gmail.com>
2026-05-16 18:25:37 +0700

    feat: implement DTO request classes for product and category management with validation constraints

---
+++ Product-service/src/main/java/com/ice/productservice/DTO/Request/Product/CreateVariantProductRequest.java
@@ -0,0 +1,20 @@
+package com.ice.productservice.DTO.Request.Product;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateVariantProductRequest {
+    private String sku;
+    private String color;
+    private String size;
+    private Long price;
+    private Integer stockQty;
+    private String imageUrl;
+}
