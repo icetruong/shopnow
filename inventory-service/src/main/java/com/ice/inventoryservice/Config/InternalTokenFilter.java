c5d2d0c28ac3df814810c48c265dd13bb18720df
icetruong <hocthidauthoi@gmail.com>
2026-05-18 14:00:47 +0700

    feat: implement security filters, global exception handling, and product category controller.

---
+++ inventory-service/src/main/java/com/ice/inventoryservice/Config/InternalTokenFilter.java
@@ -0,0 +1,4 @@
+package com.ice.inventoryservice.Config;
+
+public class InternalTokenFilter {
+}
---
+++ inventory-service/src/main/java/com/ice/inventoryservice/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.ice.inventoryservice.Exception;
+
+public class GlobalExceptionHandler {
+}
