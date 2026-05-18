0f0a8019e6852d1be8a233281104889204d0c263
icetruong <hocthidauthoi@gmail.com>
2026-05-18 14:46:38 +0700

    feat: initialize inventory service infrastructure with repositories, controllers, and API documentation

---
+++ inventory-service/src/main/java/com/ice/inventoryservice/Controller/Internal/InternalInventoryController.java
@@ -0,0 +1,4 @@
+package com.ice.inventoryservice.Controller.Internal;
+
+public class InventoryController {
+}
---
+++ inventory-service/src/main/java/com/ice/inventoryservice/Repository/InventoryRepo.java
@@ -0,0 +1,4 @@
+package com.ice.inventoryservice.Repository;
+
+public interface InventoryRepo {
+}
