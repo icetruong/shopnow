23f09df992ceb7f527a608bb91a233d7ad1834d3
icetruong <hocthidauthoi@gmail.com>
2026-06-06 17:17:48 +0700

    feat: implement cart retrieval and internal product batch synchronization services

---
+++ cart-service/src/main/java/com/ice/cartservice/Exception/ServiceUnavailableException.java
@@ -0,0 +1,7 @@
+package com.ice.cartservice.Exception;
+
+public class ServiceUnavailableException extends RuntimeException {
+  public ServiceUnavailableException(String message) {
+    super(message);
+  }
+}
