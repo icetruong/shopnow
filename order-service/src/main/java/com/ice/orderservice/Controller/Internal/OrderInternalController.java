992849887de21f7cb567750cfa4f38b2a442bda0
icetruong <hocthidauthoi@gmail.com>
2026-08-24 19:00:42 +0700

    feat: implement admin order management API and global exception handling support

---
+++ order-service/src/main/java/com/ice/orderservice/Controller/Internal/OrderInternalController.java
@@ -0,0 +1,4 @@
+package com.ice.orderservice.Controller.Internal;
+
+public class OrderInternalController {
+}
---
+++ order-service/src/main/java/com/ice/orderservice/DTO/Request/Order/AdminUpdateStatusOrderRequest.java
@@ -0,0 +1,21 @@
+package com.ice.orderservice.DTO.Request.Order;
+
+import com.ice.orderservice.Enum.OrderStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminUpdateStatusOrder {
+    @NotNull(message = "status must be not null")
+    private OrderStatus status;
+
+    @NotBlank(message = "note must be not blank")
+    private String note;
+}
