814cb3429570e2a2844248f625970d6b131e068e
icetruong <hocthidauthoi@gmail.com>
2026-08-26 11:08:10 +0700

    feat: integrate payment client into OrderService for multi-gateway payment processing

---
+++ order-service/src/main/java/com/ice/orderservice/Exception/PaymentCreationFailedException.java
@@ -0,0 +1,7 @@
+package com.ice.orderservice.Exception;
+
+public class PaymentCreationFailedException extends RuntimeException {
+  public PaymentCreationFailedException(String message) {
+    super(message);
+  }
+}
