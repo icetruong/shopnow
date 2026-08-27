564f36c9581569c33407a27895f5fae772043ae1
icetruong <hocthidauthoi@gmail.com>
2026-08-27 08:06:02 +0700

    feat: initialize shipping-service project and add event listener DTOs to order-service

---
+++ order-service/src/main/java/com/ice/orderservice/DTO/Event/Cosume/PaymentRefundPayload.java
@@ -0,0 +1,4 @@
+package com.ice.orderservice.DTO.Event.Cosume;
+
+public class PaymentRefundPayload {
+}
---
+++ order-service/src/main/java/com/ice/orderservice/Listener/PaymentRefundListener.java
@@ -0,0 +1,4 @@
+package com.ice.orderservice.Listener;
+
+public class PaymentRefundListener {
+}
