aedd45f8dfa8daec6a1660a0cbe405af7133f1b4
icetruong <hocthidauthoi@gmail.com>
2026-08-28 17:35:40 +0700

    feat: implement order cancellation listener and service logic to handle shipping order cancellations

---
+++ shipping-service/src/main/java/com/ice/shippingservice/DTO/Event/Consume/OrderCancelledPayload.java
@@ -0,0 +1,19 @@
+package com.ice.orderservice.DTO.Event.Publish;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderCancelledPayload {
+    private String orderId;
+    private String reason;
+    private Boolean needReleaseStock;
+    private List<OrderItemEvent> items;
+}
---
+++ shipping-service/src/main/java/com/ice/shippingservice/Listener/OrderCancelledListener.java
@@ -0,0 +1,4 @@
+package com.ice.shippingservice.Listener;
+
+public class OrderCancelledListener {
+}
