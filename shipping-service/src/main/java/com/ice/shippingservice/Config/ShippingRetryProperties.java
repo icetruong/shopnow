c64b26e307e33b9aa7b886e481787ef5ae1740a0
icetruong <hocthidauthoi@gmail.com>
2026-08-29 08:48:53 +0700

    feat: implement shipping service core logic, admin management endpoints, and background retry scheduling

---
+++ shipping-service/src/main/java/com/ice/shippingservice/Config/ShippingRetryProperties.java
@@ -0,0 +1,4 @@
+package com.ice.shippingservice.Config;
+
+public class ShippingRetryPropertiess {
+}
---
+++ shipping-service/src/main/java/com/ice/shippingservice/Schedule/PendingShipmentRetryJob.java
@@ -0,0 +1,4 @@
+package com.ice.shippingservice.Schedule;
+
+public class PendingJobRetrySchedule {
+}
