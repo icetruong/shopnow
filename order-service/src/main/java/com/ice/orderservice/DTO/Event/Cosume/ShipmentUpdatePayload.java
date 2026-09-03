88b81afb06597ad24c49b651e146f59249fc3171
icetruong <hocthidauthoi@gmail.com>
2026-09-03 20:35:53 +0700

    feat: implement order saga recovery scheduler and database locking mechanisms

---
+++ order-service/src/main/java/com/ice/orderservice/DTO/Event/Cosume/ShipmentUpdatePayload.java
@@ -0,0 +1,22 @@
+package com.ice.shippingservice.DTO.Event.Publish;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ShipmentUpdatePayload {
+    private String orderId;
+    private String shipmentId;
+    private String trackingCode;
+    private String carrier;
+    private String status;
+    private String description;
+    private LocalDate estimatedDate;
+}
